8076b5cf3f454485fd2fdedd222803c22556370c
Nail <nil20122013@gmail.com>
2019-11-06 12:17:38 +0500

    Added separate class for fridays

---
+++ src/test/NearestFridayTest.java
@@ -0,0 +1,130 @@
+package test;
+
+import weekend.NearestFriday;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
+public class NearestFridayTest {
+
+	private static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss.SSS");
+
+	@Test
+	@Parameters(method = "data_friday_DuringWholeWeek_IsCorrect")
+	public void friday_DuringWholeWeek_IsCorrect(String expectation, String monday) {
+		try {
+			var friday = new NearestFriday(DATE_FORMATTER.parse(monday));
+			assertEquals(DATE_FORMATTER.parse(expectation), friday.friday);
+		} catch (ParseException ex) {}
+	}
+
+	@Test
+	@Parameters(method = "data_friday_WhenFriday_IsCorrect")
+	public void friday_WhenFriday_IsCorrect(String expectation, String suppliedFriday) {
+		try {
+			var friday = new NearestFriday(DATE_FORMATTER.parse(suppliedFriday));
+			assertEquals(DATE_FORMATTER.parse(expectation), friday.friday);
+		} catch (ParseException ex) {}
+	}
+
+	@Test
+	@Parameters(method = "data_friday_WhenEndOfMonth_IsCorrect")
+	public void friday_WhenEndOfMonth_IsCorrect(String expectation, String actual) {
+		try {
+			var friday = new NearestFriday(DATE_FORMATTER.parse(actual));
+			assertEquals(DATE_FORMATTER.parse(expectation), friday.friday);
+		} catch (ParseException ex) {}
+	}
+
+	public void friday_WhenEndOfYear_IsCorrect() {}
+	public void friday_WhenWeekend_IsNull() {}
+	public void friday_WhenEndOfFriday_IsNull() {}
+
+	// --------------------------------------------------------------
+
+	public Object[][] data_friday_DuringWholeWeek_IsCorrect() {
+		return new String[][]{
+			new String[]{
+				"08.11.2019 18:00:00.000", "04.11.2019 08:55:35.432"
+			},
+			new String[]{
+				"08.11.2019 18:00:00.000", "05.11.2019 13:55:00.432"
+			},
+			new String[]{
+				"08.11.2019 18:00:00.000", "06.11.2019 18:25:00.000"
+			},
+			new String[]{
+				"08.11.2019 18:00:00.000", "07.11.2019 00:00:00.000"
+			},
+			new String[]{
+				"29.11.2019 18:00:00.000", "25.11.2019 00:00:00.000"
+			},
+			new String[]{
+				"29.11.2019 18:00:00.000", "26.11.2019 01:00:12.200"
+			},
+			new String[]{
+				"29.11.2019 18:00:00.000", "27.11.2019 16:04:12.200"
+			},
+			new String[]{
+				"29.11.2019 18:00:00.000", "28.11.2019 17:59:59.999"
+			},
+		};
+	}
+
+	public Object[][] data_friday_WhenFriday_IsCorrect() {
+		return new String[][]{
+			new String[]{
+				"08.11.2019 18:00:00.000", "08.11.2019 08:55:35.432"
+			},
+			new String[]{
+				"08.11.2019 18:00:00.000", "08.11.2019 13:55:35.432"
+			},
+			new String[]{
+				"08.11.2019 18:00:00.000", "08.11.2019 17:59:59.999"
+			},
+			new String[]{
+				"08.11.2019 18:00:00.000", "08.11.2019 17:59:59.000"
+			},
+			new String[]{
+				"08.11.2019 18:00:00.000", "08.11.2019 17:59:01.000"
+			},
+			new String[]{
+				"29.11.2019 18:00:00.000", "29.11.2019 08:55:35.432"
+			},
+			new String[]{
+				"29.11.2019 18:00:00.000", "29.11.2019 13:55:35.432"
+			},
+			new String[]{
+				"29.11.2019 18:00:00.000", "29.11.2019 17:59:59.999"
+			},
+			new String[]{
+				"29.11.2019 18:00:00.000", "29.11.2019 17:59:59.000"
+			},
+			new String[]{
+				"29.11.2019 18:00:00.000", "29.11.2019 17:59:01.000"
+			},
+		};
+	}
+
+	public Object[][] data_friday_WhenEndOfMonth_IsCorrect() {
+		return new String[][]{
+			new String[]{
+				"01.11.2019 18:00:00.000", "30.10.2019 13:45:33.443"
+			},
+			new String[]{
+				"04.10.2019 18:00:00.000", "30.09.2019 13:45:33.443"
+			},
+			new String[]{
+				"02.08.2019 18:00:00.000", "29.07.2019 13:45:33.443"
+			},
+		};
+	}
+}
---
+++ src/weekend/NearestFriday.java
@@ -0,0 +1,40 @@
+package weekend;
+
+import java.util.Date;
+
+public class NearestFriday {
+
+	public final Date friday;
+
+	private static final int FRIDAY = 5;
+	private static final int SATURDAY = 6;
+	private static final int SUNDAY = 0;
+
+	public NearestFriday(Date dateFrom) {
+		this.friday = this.getNextFriday(dateFrom);
+	}
+
+	/**
+	 * Returns next nearest friday on a week.
+	 * If it is a weekend, i.e. saturday or sunday,
+	 * then it will return null.
+	 * @param dateFrom Date from which next friday is calculated.
+	 * @return Nearest next friday or null if it is weekend.
+	 *         If date is returned, then its time is set to 18:00:00.
+	 */
+	private Date getNextFriday(Date dateFrom) {
+		int day = dateFrom.getDay();
+		boolean isWeekend = day == SATURDAY || day == SUNDAY;
+		boolean isEndOfFriday = day == FRIDAY && dateFrom.getHours() >= 18;
+		if (isWeekend || isEndOfFriday)
+			return null;
+		int daysToAdd = FRIDAY - day;
+		var nextFriday = new Date();
+		nextFriday.setDate(dateFrom.getDate() + daysToAdd);
+		nextFriday.setHours(18);
+		nextFriday.setMinutes(0);
+		nextFriday.setSeconds(0);
+		nextFriday.setTime(nextFriday.getTime() / 1000 * 1000);
+		return nextFriday;
+	}
+}
